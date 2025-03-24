5b0ef798e00ef812611c9761b3e1b23df214b714
calluna <106458302+callunalessthan3@users.noreply.github.com>
2025-03-23 20:45:45 -0700

    implement end, recursive opt, noconfirm opt, fix improper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
+//:TODO: wherever FSWAP DIR is specified, make it current directory if unspecified
+
 extern crate pathdiff;
 
 use std::env;
 use std::str;
 use std::process::exit;
 use std::ffi::OsString;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
 use std::collections::VecDeque;
+use std::path::{Path, PathBuf};
 use std::fs::{self, File, DirEntry};
+use std::io::{self, Read, Write};
 
 const FSWAP_EXT: &'static str = ".fswap";
 
-use bitflags::bitflags;
-bitflags! {
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-    pub struct Opts: u32 {
-        const NONE = 0;
-        const ALL_FILES = 1 << 1;
-        const HELP      = 1 << 2;
-        const RECURSIVE = 1 << 3;
-    }
-}
-
-
 fn cmd_begin(u_input: &mut UserInput) {
-
     let arg = u_input.next_arg();
     let source_dir = Path::new(&arg);
     let arg = u_input.next_arg();
@@ -41,7 +30,7 @@ fn cmd_begin(u_input: &mut UserInput) {
         exit(1);
     }
 
-    // Check files aren't directories
+    // Check files are directories
     let source_md = source_dir.metadata().unwrap_or_else(|err| {
         eprintln!("ERROR: Couldn't get metadata from '{dir}': {err}", dir = source_dir.display());
         exit(1);
@@ -70,23 +59,38 @@ fn cmd_begin(u_input: &mut UserInput) {
         exit(1);
     });
 
-    let path_diff = pathdiff::diff_paths(source_dir, working_dir).expect("pathdiff shouldnt fail");
-    if let Err(err) = write!(fswap_file, "{}", path_diff.as_path().display()) {
+    let path_diff = pathdiff::diff_paths(source_dir, working_dir).unwrap_or_else(|| {
+        eprintln!("ERROR: Source directory and fswap directory cannot be the same.");
+        exit(1);
+    });
+
+    if let Err(err) = write!(fswap_file, "{src}", src = path_diff.as_path().display()) {
         eprintln!("ERROR: couldn't write to '{file}': {err}", file = fswap_path.display());
         exit(1);
     }
 
+    if !u_input.opts.silent {
+        println!("INFO: Created file '{file}', with path to source '{path}'.", file = fswap_path.display(), path = path_diff.display());
+    }
 }
 
 
-fn find_fswap_files(path: &PathBuf) -> Option<Vec<String>> {
+// NOTE: will not find file ".fswap" if in FSWAP is the current directory (used internally)
+fn find_files_with(path: &PathBuf, contains: Option<&str>) -> Option<Vec<String>> {
+    let contains = match contains {
+        Some(x) => x,
+        None    => &"",
+    };
+
     let all_files = fs::read_dir(&path).unwrap_or_else(|err| {
         eprintln!("ERROR: Couldn't read dir '{dir}': {err}", dir = path.display());
         exit(1);
     });
 
-    // NOTE: failing here shouldn't be my problem
-    let mut files: VecDeque<DirEntry> = VecDeque::from(all_files.map(|x| x.unwrap()).collect::<Vec<DirEntry>>());
+    let mut files: VecDeque<DirEntry> = VecDeque::from(all_files.map(|x| x.unwrap_or_else(|err| {
+        eprintln!("ERROR: Unhandleable fs::DirEntry error: {err}");
+        exit(1);
+    })).collect::<Vec<DirEntry>>());
     let mut fswap_files: Vec<String> = vec![];
 
     loop {
@@ -106,11 +110,17 @@ fn find_fswap_files(path: &PathBuf) -> Option<Vec<String>> {
                 exit(1);
             });
 
-            sub_files.into_iter().for_each(|file| files.push_back(file.unwrap()));
+            sub_files.into_iter().for_each(|file| files.push_back(file.unwrap_or_else(|err| {
+                eprintln!("ERROR: Unhandleable fs::DirEntry error: {err}");
+                exit(1);
+            })));
         } else {
-            let file_path = file.path();                     // idk why this is necessary at all, but borrow checker doesnt like it if i dont do this
-            let file_path_str = file_path.to_str().unwrap(); // NOTE: failing here shouldn't be my problem
-            if file_path_str.contains(FSWAP_EXT) && !file_path_str.eq("./.fswap") {
+            let file_path = file.path(); // idk why this is necessary at all, but borrow checker doesnt like it if i dont do this
+            let file_path_str = file_path.to_str().unwrap_or_else(|| {
+                eprintln!("ERROR: '{file}' is invalid unicode.", file = file_path.display());
+                exit(1);
+            });
+            if file_path_str.contains(contains) && !file_path_str.eq("./.fswap") {
                 fswap_files.push(String::from(file_path_str));
             }
 
@@ -133,22 +143,23 @@ fn cmd_info(u_input: &mut UserInput) {
         arg = String::from(".");
     }
 
-    let working_dir = PathBuf::from(arg);
+    let mut working_dir = PathBuf::from(arg);
 
-    let mut fswap_file = working_dir.clone();
-    fswap_file.push(FSWAP_EXT);
-    if let Err(err) = File::open(&fswap_file) {
-        eprintln!("ERROR: Couldn't open '{FSWAP_EXT}': {err}");
+    working_dir.push(FSWAP_EXT);
+    if let Err(err) = File::open(&working_dir) {
+        eprintln!("ERROR: Couldn't open '{file}': {err}", file = working_dir.display());
         exit(1);
     }
 
-    match find_fswap_files(&working_dir) {
+    working_dir.pop();
+
+    match find_files_with(&working_dir, Some(FSWAP_EXT)) {
         Some(paths) => {
-            println!("fswap files in '{dir}'", dir = working_dir.display());
-            paths.iter().for_each(|x| println!("{x}"));
+            println!("fswap files in '{dir}':", dir = working_dir.display());
+            paths.iter().for_each(|x| println!("  {x}"));
         },
         None => {
-            eprintln!("No fswap files found.");
+            println!("No fswap files in '{dir}'.", dir = working_dir.display());
         },
     };
 }
@@ -156,7 +167,7 @@ fn cmd_info(u_input: &mut UserInput) {
 
 fn cmd_help(_u_input: &mut UserInput) {
 
-    let help = format!( r#"Usage: fswap [COMMAND | help] [OPTIONS...] [ARGUMENTS...]
+    let help = format!( r#"Usage: fswap [OPTIONS...] [COMMAND | help] [ARGUMENTS...]
 Note: must be in FSWAP DIR for all commands that do not take it as an argument
 
 COMMANDS
@@ -169,15 +180,79 @@ COMMANDS
 OPTIONS
   -a, --all          do command to all files in fswap directory
   -h, --help         print this help
+  -n, --noconfirm    will not ask for confirmation to delete files when end is ran
   -r, --recursive    do command to all files in directory
-"#);
+  -s, --silent       suppress output of command"#);
 
     println!("{}", help);
 }
 
 
-fn cmd_end(_u_input: &mut UserInput) {
-    todo!();
+fn cmd_end(u_input: &mut UserInput) {
+    let arg = u_input.next_arg();
+    let mut path = PathBuf::from(&arg);
+
+    path.push(FSWAP_EXT);
+    if !path.exists() {
+        eprintln!("ERROR: '{file}' doesn't exist.", file = path.display());
+        exit(1);
+    }
+
+    if !u_input.opts.noconfirm {
+        let stdin = io::stdin();
+        let mut stdout = io::stdout();
+        loop {
+            print!("CONFIRM: Delete all files with the suffix '{FSWAP_EXT}'? [y/N] ");
+            stdout.flush().unwrap_or_else(|err| {
+                eprintln!("ERROR: Unhandleable io::stdout() error: {err}");
+                exit(1);
+            });
+            let mut buf = String::new();
+            match stdin.read_line(&mut buf) {
+                Ok(n) if n == 2 => {
+                    let buf = buf.to_ascii_lowercase();
+                         if buf.eq("y\n") { break; }
+                    else if buf.eq("n\n") { exit(1); }
+                    else { continue; }
+                },
+                Ok(n) => {
+                    eprintln!("buf = {buf}\nn = {n}");
+                    continue;
+                }
+                Err(err) => {
+                    eprintln!("ERROR: Couldn't read line: {err}");
+                    exit(1);
+                },
+            };
+        }
+    }
+
+    if let Err(err) = fs::remove_file(&path) {
+        eprintln!("ERROR: Couldn't delete file '{FSWAP_EXT}': {err}");
+        exit(1);
+    }
+
+    if !u_input.opts.silent {
+        println!("INFO: Deleted '{file}'.", file = path.display());
+    }
+
+    path.pop();
+
+    let files = match find_files_with(&path, Some(FSWAP_EXT)) {
+        Some(x) => x,
+        None    => return,
+    };
+
+    for file in files {
+        if let Err(err) = fs::remove_file(&file) {
+            eprintln!("ERROR: Couldn't delete '{file}': {err}");
+            exit(1);
+        }
+
+        if !u_input.opts.silent {
+            println!("INFO: Deleted '{file}'.");
+        }
+    }
 }
 
 
@@ -194,7 +269,10 @@ fn cmd_swap(u_input: &mut UserInput) {
         exit(1);
     });
 
-    let sd = str::from_utf8(&buf).unwrap(); // NOTE: failing here shouldn't be my problem
+    let sd = str::from_utf8(&buf).unwrap_or_else(|err| {
+        eprintln!("ERROR: Unhandleable str::from_utf8 error: {err}");
+        exit(1);
+    });
     let source_dir = Path::new(&sd[0..sd_len]);
 
     if !source_dir.exists() {
@@ -202,19 +280,33 @@ fn cmd_swap(u_input: &mut UserInput) {
         exit(1);
     }
 
-    let mut source_file = PathBuf::from(source_dir);
-
     let files: Vec<String>;
-    if !u_input.opts.intersection(Opts::ALL_FILES).is_empty() {
-        let swapped_files = match find_fswap_files(&PathBuf::from(".")) {
+    if u_input.opts.all {
+        let swapped_files = match find_files_with(&PathBuf::from("."), None) {
             Some(x) => x,
             None    => {
                 eprintln!("No fswap files found.");
-                exit(1);
+                return;
             },
         };
 
-        files = swapped_files.iter().map(|x| x.strip_suffix(FSWAP_EXT).unwrap().to_string()).collect();
+        files = swapped_files;
+    } else if u_input.opts.recursive {
+        let dirs = &u_input.args;
+        let mut out_files: Vec<String> = vec![];
+        for dir in dirs {
+            let swapped_files = match find_files_with(&PathBuf::from(&dir), None) {
+                Some(x) => x,
+                None    => {
+                    eprintln!("No fswap files found.");
+                    return;
+                },
+            };
+
+            out_files = combine_string_vecs(&out_files, &swapped_files);
+        }
+
+        files = out_files;
     } else {
         files = u_input.args.clone();
     }
@@ -227,6 +319,7 @@ fn cmd_swap(u_input: &mut UserInput) {
         };
 
         let working_file = PathBuf::from(&arg);
+        let mut source_file = PathBuf::from(source_dir);
         source_file.push(&working_file);
 
         if !working_file.exists() {
@@ -261,7 +354,10 @@ fn cmd_swap(u_input: &mut UserInput) {
             exit(1);
        }
 
-        source_file.pop();
+       if !u_input.opts.silent {
+           println!("INFO: Renamed '{src}' -> '{dest}'.", src = working_file.display(), dest = swapped_file.display());
+           println!("INFO: Copied '{src}' -> '{dest}'.", src = source_file.display(), dest = working_file.display());
+       }
     }
 }
 
@@ -273,16 +369,33 @@ fn cmd_revert(u_input: &mut UserInput) {
     }
 
     let files: Vec<String>;
-    if !u_input.opts.intersection(Opts::ALL_FILES).is_empty() {
-        let swapped_files = match find_fswap_files(&PathBuf::from(".")) {
+    if u_input.opts.all {
+        let swapped_files = match find_files_with(&PathBuf::from("."), Some(FSWAP_EXT)) {
             Some(x) => x,
             None    => {
                 eprintln!("No fswap files found.");
-                exit(1);
+                return;
             },
         };
 
-        files = swapped_files.iter().map(|x| x.strip_suffix(FSWAP_EXT).unwrap().to_string()).collect();
+        files = swapped_files.iter().map(|x| x.strip_suffix(FSWAP_EXT).expect("Files that contain .fswap should always have it as a suffix").to_string()).collect();
+    } else if u_input.opts.recursive {
+        let dirs = &u_input.args;
+        let mut out_files: Vec<String> = vec![];
+        for dir in dirs {
+            let swapped_files = match find_files_with(&PathBuf::from(&dir), Some(FSWAP_EXT)) {
+                Some(x) => x,
+                None    => {
+                    eprintln!("No fswap files found.");
+                    return;
+                },
+            };
+
+            let cur_files = swapped_files.iter().map(|x| x.strip_suffix(FSWAP_EXT).expect("Files that contain .fswap should always have it as a suffix").to_string()).collect();
+            out_files = combine_string_vecs(&out_files, &cur_files);
+        }
+
+        files = out_files;
     } else {
         files = u_input.args.clone();
     }
@@ -315,7 +428,19 @@ fn cmd_revert(u_input: &mut UserInput) {
             eprintln!("ERROR: Couldn't rename '{work_file}' to '{src_file}': {err}", work_file = working_file.display(), src_file = source_file.display());
             exit(1);
         }
+
+       if !u_input.opts.silent {
+           println!("INFO: Removed '{file}'.", file = source_file.display());
+           println!("INFO: Renamed '{src}' -> '{dest}'.", src = working_file.display(), dest = source_file.display());
+       }
     }
+}
+
+
+fn combine_string_vecs(a: &Vec<String>, b: &Vec<String>) -> Vec<String> {
+    let mut ret = a.clone();
+    b.iter().for_each(|x| ret.push(x.to_string()));
+    return ret;
 }
 
 
@@ -337,7 +462,27 @@ fn _strip_suffix_from_pathbuf(pb: &PathBuf, ext: &str) -> Option<PathBuf> {
 }
 
 
-// idk if this level of abstractions is even good, but did it 4 learning purposes
+struct Opts {
+    all: bool,
+    help: bool,
+    noconfirm: bool,
+    recursive: bool,
+    silent: bool,
+}
+
+impl Opts {
+    fn new() -> Self {
+        Self {
+            all: false,
+            help: false,
+            noconfirm: false,
+            recursive: false,
+            silent: false,
+        }
+    }
+}
+
+// idk if this level of abstraction is even good, but did it 4 learning purposes
 struct UserInput {
     args: Vec<String>,
     argc: usize,
@@ -350,7 +495,7 @@ impl UserInput {
         let mut args_in: Vec<String> = env::args().collect();
         args_in.remove(0);
         let opts_out = Self::args_to_flags(&args_in);
-        let args_out = Self::strip_args_opts(&args_in);
+        let args_out = Self::strip_opts_from_args(&args_in);
         let argc_out = args_out.len();
 
         return Self {
@@ -373,19 +518,17 @@ impl UserInput {
 
 
     fn usage() {
-        // TODO: print usage based on current command
         println!("Usage: fswap [COMMAND] [OPTIONS...] [ARGUMENTS...]\nSee 'fswap --help' for more information.");
     }
 
-
-    fn strip_args_opts(args: &Vec<String>) -> Vec<String> {
-        // NOTE: '⟡' kinda looks like a face
+    fn strip_opts_from_args(args: &Vec<String>) -> Vec<String> {
+        // IMPORTANT NOTE: '⟡' looks like a face
         return args.clone().into_iter().filter(|x| x.chars().next().unwrap_or('⟡') != '-').collect();
     }
 
 
     fn args_to_flags(args: &Vec<String>) -> Opts {
-        let mut flags = Opts::NONE;
+        let mut opts = Opts::new();
         for arg in args {
             let mut chars = arg.chars().peekable();
             match chars.next() {
@@ -393,19 +536,20 @@ impl UserInput {
                 _ => continue,
             };
 
-            // peek here bc its valid in case of short opt
+            // peek here so it wont consume an important character valid in case of short opt
             let long_opt = match chars.peek() {
                 Some(c) if *c == '-' => true,
                 _ => false,
             };
 
-            // can add arg parsing post option here
             if !long_opt {
                 loop {
                     match chars.next() {
-                        Some(c) if c == 'd' => flags = flags.union(Opts::RECURSIVE),
-                        Some(c) if c == 'a' => flags = flags.union(Opts::ALL_FILES),
-                        Some(c) if c == 'h' => flags = flags.union(Opts::HELP),
+                        Some(c) if c == 'a' => opts.all = true,
+                        Some(c) if c == 'h' => opts.help = true,
+                        Some(c) if c == 'n' => opts.noconfirm = true,
+                        Some(c) if c == 'r' => opts.recursive = true,
+                        Some(c) if c == 's' => opts.silent = true,
                         Some(_) => {
                             eprintln!("ERROR: Invalid options '{arg}'");
                             exit(1);
@@ -415,9 +559,11 @@ impl UserInput {
                 }
             } else {
                 match arg.as_str() {
-                    "--directory" => flags = flags.union(Opts::RECURSIVE),
-                    "--all"       => flags = flags.union(Opts::ALL_FILES),
-                    "--help"      => flags = flags.union(Opts::HELP),
+                    "--all"       => opts.all = true,
+                    "--help"      => opts.help = true,
+                    "--noconfirm" => opts.noconfirm = true,
+                    "--recursive" => opts.recursive = true,
+                    "--silent"    => opts.silent = true,
                     _ => {
                         eprintln!("ERROR: Invalid options '{arg}'");
                         exit(1);
@@ -426,7 +572,7 @@ impl UserInput {
             }
         }
 
-        return flags;
+        return opts;
     }
 }
 
@@ -438,8 +584,9 @@ struct Command {
 }
 
 
-const NUM_COMMANDS: usize = 6;
-const COMMANDS: [Command; NUM_COMMANDS] = [
+// code style inspired by https://github.com/rexim/tore
+// tool usage inspired by nmcli
+const COMMANDS: [Command; 6] = [
     Command {
         name: "begin",
         short: "b",
@@ -478,11 +625,11 @@ const COMMANDS: [Command; NUM_COMMANDS] = [
 ];
 
 
-// Commands either succeed or exit the program with non-zero exit code
+// Commands either succeed and return, or exit the program with non-zero exit code
 fn main() {
     let mut u_input = UserInput::new();
 
-    if !u_input.opts.intersection(Opts::HELP).is_empty() {
+    if u_input.opts.help {
         cmd_help(&mut u_input);
         exit(0);
     }
